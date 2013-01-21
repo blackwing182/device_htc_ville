CyanogenMod/android_device_htc_ville

ville: move init scripts to common where possible
Change-Id: I61c31425f106b1216fa07ba54140f4617a7140fb

device_ville.mk
@@ -22,11 +22,9 @@ $(call inherit-product, device/htc/msm8960-common/msm8960.mk)
 DEVICE_PACKAGE_OVERLAYS += device/htc/ville/overlay
 
 # Boot ramdisk setup
-PRODUCT_COPY_FILES += \
-    device/htc/ville/ramdisk/fstab.ville:root/fstab.ville \
-    device/htc/ville/ramdisk/init.ville.rc:root/init.ville.rc \
-    device/htc/ville/ramdisk/init.ville.usb.rc:root/init.ville.usb.rc \
-    device/htc/ville/ramdisk/ueventd.ville.rc:root/ueventd.ville.rc
+PRODUCT_PACKAGES += \
+    fstab.ville \
+    init.qcom.rc
 
 # recovery and custom charging
 PRODUCT_COPY_FILES += \

rootdir/Android.mk
@@ -1,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= fstab.ville
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/fstab.ville
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.qcom.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.qcom.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)

blackwing182/device_htc_ville

device_ville.mk
@@ -22,11 +22,9 @@ $(call inherit-product, device/htc/msm8960-common/msm8960.mk)
 DEVICE_PACKAGE_OVERLAYS += device/htc/ville/overlay
 
 # Boot ramdisk setup
-PRODUCT_COPY_FILES += \
-    device/htc/ville/ramdisk/fstab.ville:root/fstab.ville \
-    device/htc/ville/ramdisk/init.ville.rc:root/init.ville.rc \
-    device/htc/ville/ramdisk/init.ville.usb.rc:root/init.ville.usb.rc \
-    device/htc/ville/ramdisk/ueventd.ville.rc:root/ueventd.ville.rc
+PRODUCT_PACKAGES += \
+    fstab.ville \
+    init.target.rc
 
 # recovery and custom charging
 PRODUCT_COPY_FILES += \

rootdir/Android.mk
@@ -1,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= fstab.ville
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/fstab.ville
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.target.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.target.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)