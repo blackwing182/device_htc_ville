0ae3436eb9535daf294d576d137144389c674184
travp624 <travp624@gmail.com>
2013-01-23 00:35:32 -0600

    Ville: fix confict with d2-common by only calling this for ville
    Signed-off-by: travp624 <travp624@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(TARGET_DEVICE),ville)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= fstab.ville
 LOCAL_MODULE_TAGS	:= optional eng
@@ -15,3 +17,5 @@ LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.target.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+endif
