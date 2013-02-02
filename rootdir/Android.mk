aa6c8e183f77ea7f9b7e99970bfea2a68f7af875
klin1344 <kevlin97@gmail.com>
2013-02-02 17:03:23 +0000

    Sync with cm-10.1 changes

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,7 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),ville)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= fstab.ville
 LOCAL_MODULE_TAGS	:= optional eng
@@ -17,5 +15,3 @@ LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.target.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
-endif
