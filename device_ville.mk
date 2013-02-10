8beef4cd16b4b03fd757ea2756c94c6aee40f223
Alan Ortiz <alan_blackwing_21@hotmail.com>
2013-02-09 23:02:11 -0400

    Update device_ville.mk
    added custom packages for ICJ

--- device_ville.mk
+++ device_ville.mk
@@ -68,9 +68,11 @@ PRODUCT_COPY_FILES += \
     device/htc/ville/idc/projector_input.idc:system/usr/idc/projector_input.idc \
     device/htc/ville/idc/tv-touchscreen.idc:system/usr/idc/tv-touchscreen.idc
 
-# Torch
+# Custom Packages
 PRODUCT_PACKAGES += \
-    Torch
+        Music \
+        CMFileManager \
+        CellBroadcastReceiver
 
 # Permissions
 PRODUCT_COPY_FILES += \
